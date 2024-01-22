ef19025ddd0199c0e3e038442d974828373307c5
MaksymJediOK <heckylmoon@gmail.com>
2024-01-22 17:23:00 +0200

    chore: fix eslint rule

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next'
 import { Jost } from 'next/font/google'
 import { Navigation } from '@/components/Navigation'
 
-const jost = Jost()
+const jost = Jost({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'PetsPaw',
